6fa8413208c5c1d5bfe95b74de49db219966a53f
Tequila-Sunrise <steven.shao6174@gmail.com>
2022-05-09 20:27:58 +0800

    Modify: adopt Mixup instead of CutMix

--- cifar-100/train.sh
+++ cifar-100/train.sh
@@ -14,11 +14,11 @@ CUDA_VISIBLE_DEVICES=0 python main.py --model=ResNet18 --batch-size=256 --lr=0.1
 # CUDA_VISIBLE_DEVICES=0 python main.py --model=ResNet18 --batch-size=256 --lr=0.1 --max-epoch=200 --use-cutmix
 
 ## Network Architecture
-# CUDA_VISIBLE_DEVICES=0 python main.py --model=ResNet50 --batch-size=256 --lr=0.1 --max-epoch=200 --use-cutmix
-# CUDA_VISIBLE_DEVICES=0 python main.py --model=PreActResNet50 --batch-size=256 --lr=0.1 --max-epoch=200 --use-cutmix
-# CUDA_VISIBLE_DEVICES=0 python main.py --model=WideResNet28x10 --batch-size=256 --lr=0.1 --max-epoch=200 --use-cutmix
-# CUDA_VISIBLE_DEVICES=0 python main.py --model=ResNeXt29_16x64d --batch-size=256 --lr=0.1 --max-epoch=200 --use-cutmix
-# CUDA_VISIBLE_DEVICES=0 python main.py --model=ResNeXt50_32x4d --batch-size=256 --lr=0.1 --max-epoch=200 --use-cutmix
-# CUDA_VISIBLE_DEVICES=0 python main.py --model=DenseNet201 --batch-size=256 --lr=0.1 --max-epoch=200 --use-cutmix
-# CUDA_VISIBLE_DEVICES=0 python main.py --model=DPN92 --batch-size=256 --lr=0.1 --max-epoch=200 --use-cutmix
-# CUDA_VISIBLE_DEVICES=0 python main.py --model=DLA --batch-size=256 --lr=0.1 --max-epoch=200 --use-cutmix
+# CUDA_VISIBLE_DEVICES=0 python main.py --model=ResNet50 --batch-size=256 --lr=0.1 --max-epoch=200 --use-mixup
+# CUDA_VISIBLE_DEVICES=0 python main.py --model=PreActResNet50 --batch-size=256 --lr=0.1 --max-epoch=200 --use-mixup
+# CUDA_VISIBLE_DEVICES=0 python main.py --model=WideResNet28x10 --batch-size=256 --lr=0.1 --max-epoch=200 --use-mixup
+# CUDA_VISIBLE_DEVICES=0 python main.py --model=ResNeXt29_16x64d --batch-size=256 --lr=0.1 --max-epoch=200 --use-mixup
+# CUDA_VISIBLE_DEVICES=0 python main.py --model=ResNeXt50_32x4d --batch-size=256 --lr=0.1 --max-epoch=200 --use-mixup
+# CUDA_VISIBLE_DEVICES=0 python main.py --model=DenseNet201 --batch-size=256 --lr=0.1 --max-epoch=200 --use-mixup
+# CUDA_VISIBLE_DEVICES=0 python main.py --model=DPN92 --batch-size=256 --lr=0.1 --max-epoch=200 --use-mixup
+# CUDA_VISIBLE_DEVICES=0 python main.py --model=DLA --batch-size=256 --lr=0.1 --max-epoch=200 --use-mixup
